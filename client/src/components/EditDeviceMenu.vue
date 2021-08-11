690b8a5416d920562e7a39f5c8dcadd7b67877f9
3rmak <r3al3rmak@gmail.com>
2021-08-11 17:28:06 +0300

    edited edit device menu

--- client/src/components/EditDeviceMenu.vue
+++ client/src/components/EditDeviceMenu.vue
@@ -8,39 +8,72 @@
         <p class="card-text">
           <b>Дата добавления: {{ new Date(device.addingDate) }}</b>
         </p>
-        <p class="card-text"><b>Мак адресс: {{ device.mac }}</b></p>
-        <p class="card-text"><b>Владелец:</b> <input type="text" value="setDefaultOwner()" :placeholder="device.owner" v-model="device.owner"></p>
-        <p class="card-text"><b>Тип:</b>
-          <select class="type-selection" v-model="device.type">
-            <option selected disabled hidden>{{device.type}}</option> 
-            <option>Компьютер</option>
-            <option>Принтер</option>
-            <option>IP-телефон</option>
-            <option>Видеонаблюдение</option>
-            <option>СКУД</option>
-            <option>Смартфон</option>
-            <option>Планшет</option>
-            <option>Ноутбук</option>
-            <hr />
-            <option>Другое</option>
-          </select>
-        </p>
-        <hr/>
-        <p class="card-text">Комментарий: 
-          <input :placeholder="device.comment" v-model="device.comment">
-        </p>
+        <div class="container-md pb-3">
+          <b>
+            <div class="row item-start">
+              <span class="col"> Мак адресс: </span>
+              <span class="col">
+                {{ device.mac }}
+              </span>
+            </div>
+          </b>
+        </div>
+        <div class="container-md pb-3">
+          <div class="row item-start">
+            <span class="col"> Владелец: </span>
+            <span class="col">
+              <input
+                type="text"
+                value="setDefaultOwner()"
+                :placeholder="device.owner"
+                v-model="device.owner"
+              />
+            </span>
+          </div>
+        </div>
+        <div class="container-md pb-3">
+          <div class="row item-start">
+            <span class="col"> Тип: </span>
+            <span class="col">
+              <select class="type-selection" v-model="device.type">
+                <option selected disabled hidden>{{ device.type }}</option>
+                <option>Компьютер</option>
+                <option>Принтер</option>
+                <option>IP-телефон</option>
+                <option>Видеонаблюдение</option>
+                <option>СКУД</option>
+                <option>Смартфон</option>
+                <option>Планшет</option>
+                <option>Ноутбук</option>
+                <hr />
+                <option>Другое</option>
+              </select>
+            </span>
+          </div>
+        </div>
+        <hr />
+        <div class="container-md pb-3">
+          <div class="form-floating">
+            <textarea class="form-control" :placeholder="device.comment" v-model="device.comment" style="height: 100px"></textarea>
+            <small style="color: gray;">*Комментарий</small>
+          </div>
+        </div>
       </div>
       <div class="modal-footer pt-0">
         <button class="btn btn-danger" @click="deleteDevice">Удалить</button>
-        <button class="btn btn-success" @click="saveChanges">Сохранить изменения</button>
-        <button class="btn btn-secondary" @click="$modal.hide(device._id)">Закрыть</button>
+        <button class="btn btn-success" @click="saveChanges">
+          Сохранить изменения
+        </button>
+        <button class="btn btn-secondary" @click="$modal.hide(device._id)">
+          Закрыть
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import request from '../assets/scripts/request'
+import request from "../assets/scripts/request";
 export default {
   props: {
     device: {
@@ -49,36 +82,35 @@ export default {
   },
   computed: {
     setDefaultOwner() {
-      return this.device.owner
-    }
+      return this.device.owner;
+    },
   },
   methods: {
     async saveChanges() {
       try {
-        const response = await request('/api/devices', "PATCH", this.device)
-        await this.$swal({text: response.message})
-          .then(()=>{
-            location.reload()
-          })
+        const response = await request("/api/devices", "PATCH", this.device);
+        await this.$swal({ text: response.message }).then(() => {
+          location.reload();
+        });
       } catch (error) {
-        await this.$swal({text: 'Не удалось применить изменения :('})
-          .then(()=>{
-            location.reload()
-          })
+        await this.$swal({ text: "Не удалось применить изменения :(" }).then(
+          () => {
+            location.reload();
+          }
+        );
       }
     },
     async deleteDevice() {
       try {
-        console.log('this.device', this.device)
-        const response = await request('/api/devices', "DELETE", this.device)
-        await this.$swal({text: response.message})
-          .then(()=>{
-            location.reload()
-          })
+        console.log("this.device", this.device);
+        const response = await request("/api/devices", "DELETE", this.device);
+        await this.$swal({ text: response.message }).then(() => {
+          location.reload();
+        });
       } catch (error) {
-        console.warn(error)
+        console.warn(error);
       }
-    }
-  }
+    },
+  },
 };
 </script>
